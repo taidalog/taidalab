62f58a24ebcd77139510ce31fbd7fee0fd07275e
taidalog <72747279+taidalog@users.noreply.github.com>
2022-05-29 01:49:02 +0900

    Added `console.log()`. (#2) close #2

--- js/program.js
+++ js/program.js
@@ -5,19 +5,25 @@ function main() {
 
         const questionBox = $('#question');
         const question = questionBox.text();
+        console.log(question);
 
         const nunmberBox = document.getElementById("nunmber_input");
         const bin = nunmberBox.value;
         const dec = parseInt(bin, destinationRadix);
+        console.log(bin);
+        console.log(dec);
 
         if (dec == question) {
             const message = "Correct!";
             $('#output').text(message);
+            console.log(message);
             const nextNumber = getRandomByte();
             questionBox.value = nextNumber;
+            console.log(nextNumber);
         } else {
             const message = "Wrong...\<br\>" + bin + "<sub>(" + destinationRadix + ")</sub> is " + dec + "<sub>(" + sourceRadix + ")</sub>.";
             $('#output').text(message);
+            console.log(message);
         }
 
 
