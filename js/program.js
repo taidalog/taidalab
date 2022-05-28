0176f64bc5d0f9fc9f2b7426a4df5237efeea120
taidalog <72747279+taidalog@users.noreply.github.com>
2022-05-29 01:23:18 +0900

    Wrote main function more. Now the value in the input box will be used, and result will be output according to the input. close #1

--- js/program.js
+++ js/program.js
@@ -1,6 +1,16 @@
 function main() {
     $('#check_button').click(function () {
-        alert("Hello, World!");
+        var user_input2 = document.getElementById("nunmber_input");
+        var bin = user_input2.value
+        var dec = parseInt(bin, 2)
+
+        var question = $('#question').text()
+
+        if (dec == question) {
+            $('#output').text("Right!");
+        } else {
+            $('#output').text("Wrong... " + bin + " is "+ dec);
+        }
     });
 }
 
