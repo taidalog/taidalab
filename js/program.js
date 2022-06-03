ad75ad98fa4ed2564f99bfa42c0b86ebfa092e42
taidalog <72747279+taidalog@users.noreply.github.com>
2022-06-03 23:45:49 +0900

    Modified style for warning message (#15).

--- js/program.js
+++ js/program.js
@@ -16,9 +16,9 @@ function main() {
     console.log(bin);
 
     if (bin == "") {
-        instruction.innerHTML = "<span class=\"result-wrong\">" + question + "の2進法表記を入力してください。</span>";
+        instruction.innerHTML = "<span class=\"warning\">" + question + "の2進法表記を入力してください。</span>";
     } else if (testBinaryString(bin) == false) {
-        instruction.innerHTML = "<span class=\"result-wrong\">\"" + bin + "\" は2進数ではありません。使えるのは半角の 0 と 1 のみです。</span>";
+        instruction.innerHTML = "<span class=\"warning\">\"" + bin + "\" は2進数ではありません。使えるのは半角の 0 と 1 のみです。</span>";
     } else {
 
         const binWithLeadingZero = colorLeadingZero(putLeadingZero(bin, digit));
