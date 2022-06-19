d31766bfc28049518222c2f1c98e9d985c97c90c
taidalog <72747279+taidalog@users.noreply.github.com>
2022-06-20 00:06:07 +0900

    Raised version to 0.8.1. Close #72.

--- README.ja.md
+++ README.ja.md
@@ -1,6 +1,6 @@
 # taidalab
 
-Version 0.8.0
+Version 0.8.1
 
 [English README](README.md)
 
--- endless-sub-add/js/main.js
+++ endless-sub-add/js/main.js
@@ -1,4 +1,4 @@
-// taidalab Version 0.8.0
+// taidalab Version 0.8.1
 // https://github.com/taidalog/taidalab
 // Copyright (c) 2022 taidalog
 // This software is licensed under the MIT License.
