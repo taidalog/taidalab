370b93191328e3bfd7e82701933801b40cfc1ed2
taidalog <72747279+taidalog@users.noreply.github.com>
2022-06-26 02:28:14 +0900

    Edited main function to recieve arguments. (#75)

--- endless-power-of-two-1/js/main.js
+++ endless-power-of-two-1/js/main.js
@@ -3,29 +3,23 @@
 // Copyright (c) 2022 taidalog
 // This software is licensed under the MIT License.
 // https://github.com/taidalog/taidalab/blob/main/LICENSE
-function main() {
+function checkNumber (index_number, number, hint_format) {
     const sourceRadix = 10;
     const destinationRadix = 2;
     const binaryDigit = 8;
     const decimalDigit = 3;
-
+    
     const instructionArea = document.getElementById('instructionArea');
     instructionArea.innerHTML = "<br>";
 
-    const questionSpan = document.getElementById('questionSpan');
-    const question = questionSpan.innerText;
-    console.log(question);
-
-    
     const numberInput = document.getElementById("numberInput");
     const bin = escapeHtml(numberInput.value);
     console.log(bin);
-    
-    const indexNumber = Math.log(question) / Math.log(2);
-    const hint = formatString(hintFormat, [question, indexNumber]);
+
+    const hint = formatString(hint_format, [number, index_number]);
 
     if (bin == "") {
-        instructionArea.innerHTML = hint + "<br><span class=\"warning\">" + question + " の2進法表記を入力してください。</span>";
+        instructionArea.innerHTML = hint + "<br><span class=\"warning\">" + number + " の2進法表記を入力してください。</span>";
     } else if (testBinaryString(bin) == false) {
         instructionArea.innerHTML = hint + "<br><span class=\"warning\">\"" + bin + "\" は2進数ではありません。使えるのは半角の 0 と 1 のみです。</span>";
     } else {
@@ -39,7 +33,7 @@ function main() {
         const outputArea = document.getElementById("outputArea");
         
         let historyClassName = ""
-        if (dec == question) {
+        if (dec == number) {
             historyClassName = "history-correct"
         } else {
             historyClassName = "history-wrong"
@@ -52,13 +46,13 @@ function main() {
         console.log(msg1);
         console.log(msg2);
         
-        if (dec == question) {
+        if (dec == number) {
             let nextNumber = 0;
             let nextIndexNumber = 0;
             do {
                 nextIndexNumber = getRandomBetween(0, 7);
                 nextNumber = Math.pow(2, nextIndexNumber);
-            } while (nextNumber == question)
+            } while (nextNumber == number)
             
             const nextHint = formatString(hintFormat, [nextNumber, nextIndexNumber]);
             questionSpan.innerText = nextNumber;
@@ -66,6 +60,8 @@ function main() {
             instructionArea.innerHTML = nextHint;
             console.log(nextHint);
             numberInput.value = "";
+
+            document.getElementById('submitButton').onclick = function() { checkNumber(nextIndexNumber, nextNumber, hintFormat); return false; };
         } else {
             instructionArea.innerHTML = hint;
         }
@@ -76,7 +72,11 @@ function main() {
 
 const initIndexNumber = getRandomBetween(0, 7);
 const initNumber = Math.pow(2, initIndexNumber);
+
 const hintFormat = "<details><summary>ヒント: </summary><span class=\"history-indented\">{0}<sub>(10)</sub> = 2<sup>{1}</sup></span><br><span class=\"history-indented\">10進法で2<sup>n</sup>になる数は、</span><br><span class=\"history-indented\">2進法では1の後ろに0をn個つけます。</span></details>";
 const hint = formatString(hintFormat, [initNumber, initIndexNumber]);
+
 document.getElementById('questionSpan').innerText = initNumber;
 document.getElementById('instructionArea').innerHTML = hint;
+
+document.getElementById('submitButton').onclick = function() { checkNumber(initIndexNumber, initNumber, hintFormat); return false;  };
