92b6b98f1ecd7382eb53c5d7be6cc3d32a69f957
taidalog <72747279+taidalog@users.noreply.github.com>
2022-06-26 22:47:44 +0900

    Modified several things (#75) Modified things like variable names and statement orders.

--- endless-power-of-two-1/js/main.js
+++ endless-power-of-two-1/js/main.js
@@ -3,71 +3,76 @@
 // Copyright (c) 2022 taidalog
 // This software is licensed under the MIT License.
 // https://github.com/taidalog/taidalab/blob/main/LICENSE
-function checkNumber (index_number, number, hint_format) {
+function checkNumber (index_number, answer, hint_format) {
+    const inputForm = document.getElementById("inputForm");
+    const userInput = escapeHtml(inputForm.value);
+    console.log(userInput);
+
     const hintArea = document.getElementById('hintArea');
     const errorArea = document.getElementById('errorArea');
     errorArea.innerHTML = "";
     
-    const numberInput = document.getElementById("numberInput");
-    const bin = escapeHtml(numberInput.value);
-    console.log(bin);
+    // const numberInput = document.getElementById("numberInput");
+    // const bin = escapeHtml(numberInput.value);
+    // console.log(bin);
     
-    const hint = formatString(hint_format, [number, index_number]);
+    const hint = formatString(hint_format, [answer, index_number]);
     
-    if (bin == "") {
-        errorArea.innerHTML = "<span class=\"warning\">" + question + " の2進法表記を入力してください。</span>";
-    } else if (testBinaryString(bin) == false) {
-        errorArea.innerHTML = "<span class=\"warning\">\"" + bin + "\" は2進数ではありません。使えるのは半角の 0 と 1 のみです。</span>";
+    if (userInput == "") {
+        errorArea.innerHTML = "<span class=\"warning\">" + answer + " の2進法表記を入力してください。</span>";
+    } else if (testBinaryString(userInput) == false) {
+        errorArea.innerHTML = "<span class=\"warning\">\"" + userInput + "\" は2進数ではありません。使えるのは半角の 0 と 1 のみです。</span>";
     } else {
         
         const binaryDigit = 8;
-        const zeroPaddedBin = bin.padStart(binaryDigit, '0');
+        const zeroPaddedBin = userInput.padStart(binaryDigit, '0');
         const taggedBin = colorLeadingZero(zeroPaddedBin);
+        console.log(zeroPaddedBin);
         console.log(taggedBin);
         
         const destinationRadix = 2;
-        const dec = parseInt(bin, destinationRadix);
-        console.log(dec);
-        
-        const outputArea = document.getElementById("outputArea");
+        const useInputToDestRadix = parseInt(userInput, destinationRadix);
+        console.log(useInputToDestRadix);
         
         const decimalDigit = 3;
-        const spacePaddedDec = dec.toString().padStart(decimalDigit, ' ').replace(' ', '&nbsp;');
+        const spacePaddedDec = useInputToDestRadix.toString().padStart(decimalDigit, ' ').replace(' ', '&nbsp;');
         
         const sourceRadix = 10;
-        const historyMessage = newHistory((dec == number), taggedBin, sourceRadix, spacePaddedDec, destinationRadix, outputArea.innerHTML);
+        const outputArea = document.getElementById("outputArea");
+        const historyMessage = newHistory((useInputToDestRadix == answer), taggedBin, sourceRadix, spacePaddedDec, destinationRadix, outputArea.innerHTML);
         outputArea.innerHTML = historyMessage;
         
-        if (dec == number) {
-            let nextNumber = 0;
+        if (useInputToDestRadix == answer) {
             let nextIndexNumber = 0;
+            let nextAnswer = 0;
             do {
                 nextIndexNumber = getRandomBetween(0, 7);
-                nextNumber = Math.pow(2, nextIndexNumber);
-            } while (nextNumber == number)
+                nextAnswer = Math.pow(2, nextIndexNumber);
+            } while (nextAnswer == answer)
+            console.log(nextAnswer);
             
-            const nextHint = formatString(hintFormat, [nextNumber, nextIndexNumber]);
-            questionSpan.innerText = nextNumber;
-            console.log(nextNumber);
-            hintArea.innerHTML = nextHint;
+            const nextHint = formatString(hintFormat, [nextAnswer, nextIndexNumber]);
             console.log(nextHint);
-            numberInput.value = "";
+            
+            questionSpan.innerText = nextAnswer;
+            hintArea.innerHTML = nextHint;
+            inputForm.value = "";
 
-            document.getElementById('submitButton').onclick = function() { checkNumber(nextIndexNumber, nextNumber, hintFormat); return false; };
+            document.getElementById('submitButton').onclick = function() { checkNumber(nextIndexNumber, nextAnswer, hintFormat); return false; };
         } else {
             hintArea.innerHTML = hint;
         }
     }
     
-    numberInput.focus();
+    inputForm.focus();
 }
 
 function newHistory (is_correct, input, source_radix, converted_input, destination_radix, existing_results) {
-    let historyClassName = ""
+    let historyClassName = "";
     if (is_correct) {
-        historyClassName = "history-correct"
+        historyClassName = "history-correct";
     } else {
-        historyClassName = "history-wrong"
+        historyClassName = "history-wrong";
     }
     
     const msg1 = "<span class =\"" + historyClassName + "\">" + input + "<sub>(" + destination_radix + ")</sub> = " + converted_input + "<sub>(" + source_radix + ")</sub></span>";
@@ -78,12 +83,12 @@ function newHistory (is_correct, input, source_radix, converted_input, destinati
 }
 
 const initIndexNumber = getRandomBetween(0, 7);
-const initNumber = Math.pow(2, initIndexNumber);
+const initAnswer = Math.pow(2, initIndexNumber);
 
 const hintFormat = "<details><summary>ヒント: </summary><span class=\"history-indented\">{0}<sub>(10)</sub> = 2<sup>{1}</sup></span><br><span class=\"history-indented\">10進法で2<sup>n</sup>になる数は、</span><br><span class=\"history-indented\">2進法では1の後ろに0をn個つけます。</span></details>";
-const hint = formatString(hintFormat, [initNumber, initIndexNumber]);
+const hint = formatString(hintFormat, [initAnswer, initIndexNumber]);
 
-document.getElementById('questionSpan').innerText = initNumber;
+document.getElementById('questionSpan').innerText = initAnswer;
 document.getElementById('hintArea').innerHTML = hint;
 
-document.getElementById('submitButton').onclick = function() { checkNumber(initIndexNumber, initNumber, hintFormat); return false;  };
+document.getElementById('submitButton').onclick = function() { checkNumber(initIndexNumber, initAnswer, hintFormat); return false;  };
