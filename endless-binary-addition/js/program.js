cdd8d48b2f9c3437f2b50a878cc9d37ade1e018f
taidalog <72747279+taidalog@users.noreply.github.com>
2022-07-03 11:08:34 +0900

    Renewed entirely. (#52) Now it works!

--- endless-binary-addition/js/program.js
+++ endless-binary-addition/js/program.js
@@ -3,97 +3,128 @@
 // Copyright (c) 2022 taidalog
 // This software is licensed under the MIT License.
 // https://github.com/taidalog/taidalab/blob/main/LICENSE
-function main() {
-    const sourceRadix = 10;
-    const destinationRadix = 2;
-    const digit = 8;
-
-    const instructionArea = document.getElementById('instructionArea');
-    instructionArea.innerHTML = "<br>";
-
-    const questionSpan = document.getElementById('questionSpan');
-    const question = questionSpan.innerText;
-    console.log(question);
-
+function checkAnswer(answer, num1, num2) {
+    
     const numberInput = document.getElementById("numberInput");
     const bin = escapeHtml(numberInput.value);
     console.log(bin);
-
-    const indexNumber = Math.log(question + 1) / Math.log(2);
-    const hint = formatString(hintFormat, [question, question + 1, indexNumber]);
-
+    
+    const errorArea = document.getElementById('errorArea');
+    errorArea.innerHTML = "";
+    
+    const sourceRadix = 2;
     if (bin == "") {
-        instructionArea.innerHTML = hint + "<br><span class=\"warning\">" + question + " の2進法表記を入力してください。</span>";
+        errorArea.innerHTML = "<span class=\"warning\">" + num1.toString(sourceRadix) + "<small>(" + sourceRadix + ")</small>" + num2.toString(sourceRadix)+ "<small>(" + sourceRadix + ")</small>" + " の2進法表記を入力してください。</span>";
     } else if (testBinaryString(bin) == false) {
-        instructionArea.innerHTML = hint + "<br><span class=\"warning\">\"" + bin + "\" は2進数ではありません。使えるのは半角の 0 と 1 のみです。</span>";
+        errorArea.innerHTML = "<span class=\"warning\">\"" + bin + "\" は2進数ではありません。使えるのは半角の 0 と 1 のみです。</span>";
     } else {
-
+        
+        const digit = 8;
         const zeroPaddedBin = bin.padStart(digit, '0');
         const taggedBin = colorLeadingZero(zeroPaddedBin);
-        const dec = parseInt(bin, destinationRadix);
+        const dec = parseInt(bin, sourceRadix);
         console.log(taggedBin);
         console.log(dec);
         
         const outputArea = document.getElementById("outputArea");
         
         let historyClassName = ""
-        if (dec == question) {
+        if (dec == answer) {
             historyClassName = "history-correct"
         } else {
             historyClassName = "history-wrong"
         }
         
-        const msg1 = "<span class =\"" + historyClassName + "\">" + taggedBin + "<sub>(" + destinationRadix + ")</sub> = " + dec + "<sub>(" + sourceRadix + ")</sub></span>";
+        const destinationRadix = 10;
+        const msg1 = "<span class =\"" + historyClassName + "\">" + taggedBin + "<sub>(" + sourceRadix + ")</sub> = " + dec + "<sub>(" + destinationRadix + ")</sub></span>";
         const msg2 = concatinateStrings(msg1, outputArea.innerHTML);
         outputArea.innerHTML = msg2;
         console.log(msg1);
         console.log(msg2);
         
-        if (dec == question) {
-            let nextNumber = 0;
-            let nextIndexNumber = 0;
-            do {
-                nextIndexNumber = getRandomBetween(1, 8);
-                nextNumber = Math.pow(2, nextIndexNumber) - 1;
-            } while (nextNumber == question)
-            
-            const nextHint = formatString(hintFormat, [nextNumber, nextNumber + 1, nextIndexNumber]);
-            questionSpan.innerText = nextNumber;
-            console.log(nextNumber);
-            instructionArea.innerHTML = nextHint;
+        if (dec == answer) {
+            // Making next question.
+            const numbers = newNumbers();
+            setColumnAddition(numbers[0], numbers[1]);
+            console.log(numbers[0]);
+            console.log(numbers[1]);
+
+            const hintArea = document.getElementById('hintArea');
+            const nextHint = newHint();
+            hintArea.innerHTML = nextHint;
             console.log(nextHint);
+
             numberInput.value = "";
-        } else {
-            instructionArea.innerHTML = hint;
+            document.getElementById('submitButton').onclick = function () { checkAnswer((numbers[0] + numbers[1]), numbers[0], numbers[1]); return false; };
         }
     }
     
     numberInput.focus();
 }
 
-const initNumber1 = getRandomBetween(0, 255);
-let initNumber2 = 0;
+function newNumbers () {
+    const number1 = getRandomBetween(0, 255);
+    let number2 = 0;
 
-do {
-    initNumber2 = getRandomBetween(0, 255 - initNumber1);
-//    console.log("band : " + (initNumber1 & initNumber2));
-} while ((initNumber1 == initNumber2) || ((initNumber1 & initNumber2) == 0) || (initNumber1 + initNumber2 > 255))
+    do {
+        number2 = getRandomBetween(0, 255 - number1);
+    //    console.log("band : " + (number1 & number2));
+    } while ((number1 == number2) || ((number1 & number2) == 0) || (number1 + number2 > 255))
 
-const initBin1 = initNumber1.toString(2);
-const initBin2 = initNumber2.toString(2);
-console.log(initNumber1);
-console.log(initBin1);
-console.log(initNumber2);
-console.log(initBin2);
-
-for (let i = 1; i<=initBin1.length; i++) {
-    document.getElementById('firstRowDigit' + i).innerText = initBin1[initBin1.length - i];
+    console.log(number1);
+    console.log(number2);
+    console.log(number1 + number2);
+    return [number1, number2];
 }
 
-for (let i = 1; i<=initBin2.length; i++) {
-    document.getElementById('secondRowDigit' + i).innerText = initBin2[initBin2.length - i];
+
+function setColumnAddition (number1, number2) {
+    const bin1 = number1.toString(2);
+    const bin2 = number2.toString(2);
+    console.log(bin1);
+    console.log(bin2);
+    console.log((number1 + number2).toString(2));
+
+    for (let i = 1; i <= 8; i++) {
+        document.getElementById('firstRowDigit' + i).innerText = "";
+    }
+
+    for (let i = 1; i <= 8; i++) {
+        document.getElementById('secondRowDigit' + i).innerText = "";
+    }
+
+    for (let i = 1; i <= bin1.length; i++) {
+        document.getElementById('firstRowDigit' + i).innerText = bin1[bin1.length - i];
+    }
+
+    for (let i = 1; i <= bin2.length; i++) {
+        document.getElementById('secondRowDigit' + i).innerText = bin2[bin2.length - i];
+    }
 }
 
-//const hintFormat = "<details><summary>ヒント: </summary><span class=\"history-indented\">{0}<sub>(10)</sub> = {1}<sub>(10)</sub> - 1<sub>(10)</sub> = 2<sup>{2}</sup> - 1<sub>(10)</sub></span>";
-//const hint = formatString(hintFormat, [initNumber, initNumber + 1, initIndexNumber]);
-//document.getElementById('instructionArea').innerHTML = hint;
+
+function newHint () {
+    const hintFormat01 = "<details><summary>ヒント: </summary>";
+    const hintFormat02 = "<p class=\"history-indented\">";
+    const hintFormat03 = "10進数の筆算と同じように、左端から上下の数を足していきます。<br>";
+    const hintFormat04 = "0<sub>(2)</sub> + 0<sub>(2)</sub> = 0<sub>(2)</sub><br>";
+    const hintFormat05 = "0<sub>(2)</sub> + 1<sub>(2)</sub> = 1<sub>(2)</sub><br>";
+    const hintFormat06 = "1<sub>(2)</sub> + 1<sub>(2)</sub> = 10<sub>(2)</sub><br>";
+    const hintFormat07 = "1<sub>(2)</sub> + 1<sub>(2)</sub> + 1<sub>(2)</sub> = 11<sub>(2)</sub><br>";
+    const hintFormat08 = "10<sub>(2)</sub> や 11<sub>(2)</sub>のように桁が繰り上がった時は、<br>";
+    const hintFormat09 = "ひとつ左の桁に 1 を足します。<br>";
+    const hintFormat10 = "</p>";
+    const hintFormat11 = "</details>";
+    const hint = hintFormat01 + hintFormat02 + hintFormat03 + hintFormat04 + hintFormat05 + hintFormat06 + hintFormat07 + hintFormat08 + hintFormat09 + hintFormat10 + hintFormat11;
+    return hint;
+}
+
+
+// initialization
+const numbers = newNumbers();
+setColumnAddition(numbers[0], numbers[1]);
+
+const hint = newHint();
+document.getElementById('hintArea').innerHTML = hint;
+
+document.getElementById('submitButton').onclick = function () { checkAnswer((numbers[0] + numbers[1]), numbers[0], numbers[1]); return false; };
