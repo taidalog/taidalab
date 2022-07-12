25c6588b26757c4cb7b50a49612b98d67a2f01f4
taidalog <72747279+taidalog@users.noreply.github.com>
2022-07-13 00:21:07 +0900

    Added binary-complement course. Close #101.

---
+++ endless-binary-complement/js/main.js
@@ -0,0 +1,108 @@
+// taidalab Version 0.12.1
+// https://github.com/taidalog/taidalab
+// Copyright (c) 2022 taidalog
+// This software is licensed under the MIT License.
+// https://github.com/taidalog/taidalab/blob/main/LICENSE
+function checkAnswer (question, answer, last_answers) {
+    const numberInput = document.getElementById('numberInput');
+    const inputValue = escapeHtml(numberInput.value);
+    console.log('inputValue : ' + inputValue);
+    
+    const errorArea = document.getElementById('errorArea');
+    errorArea.innerHTML = '';
+
+    if (inputValue == '') {
+        const questionWithoutSpace = question.replace(' ', '');
+        errorArea.innerHTML = '<span class="warning">' + questionWithoutSpace + ' の補数を、2進法表記で入力してください。</span>';
+    } else if (tesDecimalString(inputValue) == false) {
+        errorArea.innerHTML = '<span class="warning">"' + inputValue + '" は2進数ではありません。使えるのは半角の 0 と 1 のみです。</span>';
+    } else {
+
+        const inputValueAsInt = parseInt(inputValue, 2);
+        
+        let historyClassName = ''
+        if (inputValueAsInt == answer) {
+            historyClassName = 'history-correct'
+        } else {
+            historyClassName = 'history-wrong'
+        }
+        
+        const digit = 4;
+        const zeroPaddedInputValue = inputValue.padStart(digit, '0');
+        const taggedInputValue = colorLeadingZero(zeroPaddedInputValue);
+        const sourceRadix = 2;
+        const bin = inputValueAsInt.toString(sourceRadix);
+        
+        const destinationRadix = 10;
+        const outputArea = document.getElementById('outputArea');
+        const msg1 = '<span class ="' + historyClassName + '">' + taggedInputValue + '<sub>(' + sourceRadix + ')</sub></span>';
+        const msg2 = concatinateStrings(msg1, outputArea.innerHTML);
+        outputArea.innerHTML = msg2;
+        console.log(msg1);
+        console.log(msg2);
+        
+        if (inputValueAsInt == answer) {
+            
+            let nextNumber = 0;
+            
+            console.log(last_answers);
+            do {
+                nextNumber = getRandomBetween(1, 15);
+                console.log(nextNumber);
+                console.log(last_answers.some((element) => element == nextNumber));
+            } while (last_answers.some((element) => element == nextNumber));
+            
+            const nextAnswer = 16 - nextNumber;
+            const nextBin = nextNumber.toString(sourceRadix).padStart(4, '0');
+            console.log(nextAnswer);
+            console.log(nextBin);
+            
+            document.getElementById('questionSpan').innerText = nextBin;
+            
+            const reversedBin = [...nextBin].map(x => x === "1" ? "0" : "1").join('');
+            console.log(reversedBin);
+
+            const nextHint = formatString(hintFormat, [nextBin, reversedBin]);
+            const hintArea = document.getElementById('hintArea');
+            hintArea.innerHTML = nextHint;
+            console.log(nextHint);
+            
+            numberInput.value = '';
+
+            const answersToKeep = 8;
+            const lastAnswers = [nextNumber].concat(last_answers).slice(0, answersToKeep);
+            document.getElementById('submitButton').onclick = function() { checkAnswer(nextBin, nextAnswer, lastAnswers); return false; };
+        }
+    }
+    
+    numberInput.focus();
+}
+
+
+// initialization
+const sourceRadix = 2;
+
+const initNumber = getRandomBetween(1, 15);
+const initAnswer = 16 - initNumber;
+const initBin = initNumber.toString(sourceRadix).padStart(4, '0');
+console.log(initNumber);
+console.log(initAnswer);
+console.log(initBin);
+
+const reversedBin = [...initBin].map(x => x === "1" ? "0" : "1").join('');
+console.log(reversedBin);
+
+const hintFormat01 = '<details><summary>ヒント:</summary>';
+const hintFormat02 = '<p class="history-indented">2進数の補数（2の補数）は、';
+const hintFormat03 = 'ある2進数の 0 と 1 を反転させて１を足したものです。<br>';
+const hintFormat04 = '{0} の 0 と 1 を反転させると<br>';
+const hintFormat05 = '{1} になります。<br>';
+const hintFormat06 = 'これに 1 を足したものが {0} の補数（2の補数）です。</p>';
+const hintFormat07 = '</details>';
+const hintFormat = hintFormat01 + hintFormat02 + hintFormat03 + hintFormat04 + hintFormat05 + hintFormat06 + hintFormat07;
+const hint = formatString(hintFormat, [initBin, reversedBin]);
+
+document.getElementById('questionSpan').innerText = initBin;
+document.getElementById('hintArea').innerHTML = hint;
+
+document.getElementById('submitButton').onclick = function() { checkAnswer(initBin, initAnswer, [initNumber]); return false; };
