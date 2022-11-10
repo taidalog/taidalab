a836e0b9fd4dc72f71f7effbfdad95408bb9a13e
taidalog <72747279+taidalog@users.noreply.github.com>
2022-11-11 01:11:13 +0900

    Raised version to 3.2.0. Close #165.

--- README.ja.md
+++ README.ja.md
@@ -1,6 +1,6 @@
 # taidalab
 
-Version 3.1.0
+Version 3.2.0
 
 [English README](README.md)
 
--- src/Content.fs
+++ src/Content.fs
@@ -1,4 +1,4 @@
-// taidalab Version 3.1.0
+// taidalab Version 3.2.0
 // https://github.com/taidalog/taidalab
 // Copyright (c) 2022 taidalog
 // This software is licensed under the MIT License.
@@ -189,7 +189,7 @@ module Content =
 
         let question = """<span id="questionSpan" class="question-number"></span><sub id="srcRadix"></sub> を<span id="dstRadix"></span>進法で表すと？"""
         
-        let version = "Version 3.1.0"
+        let version = "Version 3.2.0"
 
         let footer = sprintf """
             <small class="footer-container">
