63ce858a9c78e7fe491c63223674b9a6a14c60f6
taidalog <72747279+taidalog@users.noreply.github.com>
2022-11-23 22:43:49 +0900

    Created Terms.fs and  Terms module. #159

---
+++ src/Terms.fs
@@ -0,0 +1,14 @@
+// taidalab Version 3.3.1
+// https://github.com/taidalog/taidalab
+// Copyright (c) 2022 taidalog
+// This software is licensed under the MIT License.
+// https://github.com/taidalog/taidalab/blob/main/LICENSE
+namespace Taidalab
+
+module Terms =
+    let main = """
+        <p>著作権は作成者 (taidalog) が所有しています。</p>
+        <p>利用に必要な通信料等は利用者の負担となります。</p>
+        <p>当サイトを利用したことにより、コンピュータウィルス等による被害やデータの損失、その他いかなる不利益が生じた場合も、作成者は一切の責任を負いません。</p>
+        <p>ソースコードの利用は可能ですが、再頒布時には著作権表示とライセンス表示を消さずに残しておいてください。</p>
+        <p>2022年6月11日</p>"""
