b7c6ad7615f7e87c1dde06bb522f60fe096e58be
taidalog <72747279+taidalog@users.noreply.github.com>
2022-11-23 17:44:44 +0900

    Improved hints at hex2dec course. Close #173.

--- src/endless-binary/Hex2Dec.fs
+++ src/endless-binary/Hex2Dec.fs
@@ -15,11 +15,80 @@ module Hex2Dec =
         hex
         |> Seq.toList
         |> List.rev
-        |> List.mapi (fun i c -> sprintf """(16<sup>%d</sup> * %d)""" i (c |> string |> hexToDecimal))
+        |> List.mapi (fun i c -> sprintf """(%d * 16<sup>%d</sup>)""" (c |> string |> hexToDecimal)i)
         |> List.rev
         |> String.concat " + "
     
-    let rec checkAnswer answer (question : string) (last_answers : int list) (hint_format : string) =
+    let tableComponentsHex hex =
+        hex
+        |> Seq.toList
+        |> List.mapi (fun i c -> (c, (hex |> String.length) - i))
+        |> List.map (fun (c, n) -> 
+            sprintf """<span class="hint-table-digit">%d</span>""" n,
+            sprintf """<span class="hint-table-digit green large">%c</span>""" c,
+            sprintf """<span class="hint-table-digit palegreen">%d<sup>%d</sup></span>""" 16 (n - 1))
+    
+    let newHintTable (a, b, c) =
+        sprintf
+            """
+            <div class="hint-table">
+                <div class="hint-table-row">
+                    %s
+                    <div class="hint-table-container">
+                        <span class="middle">桁目</span>
+                    </div>
+                </div>
+                <div class="hint-table-row">
+                    %s
+                    <div class="hint-table-container">
+                        <span class="middle">ビット</span>
+                    </div>
+                </div>
+                <div class="hint-table-row">
+                    %s
+                    <div class="hint-table-container">
+                        <span class="middle">重み</span>
+                    </div>
+                </div>
+            </div>
+            """
+            a b c
+    
+    let hintTable hex =
+        hex
+        |> tableComponentsHex
+        |> List.fold (fun x y ->
+            applyToTuples3 (fun a1 a2 -> sprintf"%s%s" a1 a2) x y) ("", "", "")
+        |> newHintTable
+    
+    let hintFormat hex formula table =
+        sprintf
+            """<details><summary>ヒント:</summary>
+                <p class="history-indented">
+                    10進数は、一番右の桁から<br>
+                    1の位、10の位、100の位、1000の位...となっています。<br>
+                    これを「10<sup>n</sup>の位」の形で表すと、<br>
+                    10<sup>0</sup>の位、10<sup>1</sup>の位、10<sup>2</sup>の位、10<sup>3</sup>の位...となります。<br>
+                </p>
+                <p class="history-indented">
+                    同様に、16進数は一番右の桁から<br>
+                    1の位、16の位、256の位...となっています。<br>
+                    これを「16<sup>n</sup>の位」の形で表すと、<br>
+                    16<sup>0</sup>の位、16<sup>1</sup>の位、16<sup>2</sup>の位...となります。
+                </p>
+                <p class="history-indented">
+                    この 10<sup>0</sup>、10<sup>1</sup>、10<sup>2</sup>、10<sup>3</sup>...や 16<sup>0</sup>、16<sup>1</sup>、16<sup>2</sup>...という数を、その桁の「重み」と呼びます。<br>
+                    16進数を10進数に変換するには、それぞれの桁の数と重みをかけ算し、それを合計します。<br>
+                    ですので、%s<sub>(16)</sub>を10進数に変換するには、以下のように計算します。<br>
+                    ※ 16進数にA~Fのアルファベットがある場合は、それぞれ10<sub>(10)</sub>~15<sub>(10)</sub>を表しています。<br>
+                    %s
+                    <br>
+                    %s
+                </p>
+            </details>"""
+            hex formula table
+    
+    let rec checkAnswer answer (question : string) (last_answers : int list) =
         // Getting the user input.
         let numberInput = document.getElementById "numberInput" :?> Browser.Types.HTMLInputElement
         let inputValue = escapeHtml numberInput.value
@@ -72,7 +141,7 @@ module Hex2Dec =
                 (document.getElementById "questionSpan").innerText <- nextHex
                 
                 let nextAddtionFormula = writeAdditionFormulaHex nextHex
-                let nextHint = String.Format(hint_format, nextHex, nextAddtionFormula)
+                let nextHint = hintFormat nextHex nextAddtionFormula (hintTable nextHex)
                 printfn "%s" nextHint
                 
                 (document.getElementById "hintArea").innerHTML <- nextHint
@@ -85,10 +154,10 @@ module Hex2Dec =
 
                 // Setting the next answer to the check button.
                 (document.getElementById "submitButton").onclick <- (fun _ ->
-                    checkAnswer nextNumber nextHex lastAnswers hint_format
+                    checkAnswer nextNumber nextHex lastAnswers
                     false)
                 (document.getElementById "inputArea").onsubmit <- (fun _ ->
-                    checkAnswer nextNumber nextHex lastAnswers hint_format
+                    checkAnswer nextNumber nextHex lastAnswers
                     false)
 
 
@@ -101,27 +170,7 @@ module Hex2Dec =
 
         let addtionFormula = writeAdditionFormulaHex initHex
 
-        let hintFormat = """
-            <details><summary>ヒント:</summary>
-                <p class="history-indented">
-                    10進数は、一番右の桁から<br>
-                    1の位、10の位、100の位、1000の位...つまり、<br>
-                    10<sup>0</sup>の位、10<sup>1</sup>の位、10<sup>2</sup>の位、10<sup>3</sup>の位...となっています。
-                </p>
-                <p class="history-indented">
-                    同様に、16進数は一番右の桁から<br>
-                    1の位、16の位、256の位...つまり、<br>
-                    16<sup>0</sup>の位、16<sup>1</sup>の位、16<sup>2</sup>の位...となっています。
-                </p>
-                <p class="history-indented">
-                    16進数にA~Fのアルファベットがある場合は、それぞれ10~15の10進数を表しています。
-                </p>
-                <p class="history-indented">
-                    ですので、{0}<sub>(16)</sub>を10進数に変換するには、以下のように計算します。<br>
-                    {1}
-                </p>
-            </details>"""
-        let hint = String.Format(hintFormat, initHex, addtionFormula)
+        let hint = hintFormat initHex addtionFormula (hintTable initHex)
 
         let sourceRadix = 16
         let destinationRadix = 10
@@ -132,8 +181,8 @@ module Hex2Dec =
         (document.getElementById "binaryRadix").innerHTML <- sprintf "<sub>(%d)</sub>" destinationRadix
         (document.getElementById "hintArea").innerHTML <- hint
         (document.getElementById "submitButton").onclick <- (fun _ ->
-            checkAnswer initNumber initHex [initNumber] hintFormat
+            checkAnswer initNumber initHex [initNumber]
             false)
         (document.getElementById "inputArea").onsubmit <- (fun _ ->
-            checkAnswer initNumber initHex [initNumber] hintFormat
+            checkAnswer initNumber initHex [initNumber]
             false)
