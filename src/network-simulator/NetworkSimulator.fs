aacb74b0b6ff607461d78b80851faf89b943d7a2
taidalog <72747279+taidalog@users.noreply.github.com>
2023-02-21 23:36:31 +0900

    Modified to use SVG instead of bitmap images. #179
    Now emelents for devices and LAN cables are typed as HTMLElement.

--- src/network-simulator/NetworkSimulator.fs
+++ src/network-simulator/NetworkSimulator.fs
@@ -63,7 +63,7 @@ module NetworkSimulator =
         topLeft || topRight || bottomLeft || bottomRight
 
 
-    let getNetNeighbors (cables: Browser.Types.HTMLElement list) (devices: Browser.Types.HTMLImageElement list) (source: Browser.Types.HTMLElement) : Browser.Types.HTMLImageElement list =
+    let getNetNeighbors (cables: Browser.Types.HTMLElement list) (devices: Browser.Types.HTMLElement list) (source: Browser.Types.HTMLElement) : Browser.Types.HTMLElement list =
         cables
         |> List.collect
             (fun x ->
@@ -73,7 +73,22 @@ module NetworkSimulator =
     let init () =
         document.getElementById("playArea").innerHTML <- """
             <div id="device001" class="device device-note">
-              <img id="device001img" src="../image/client.png" class="device-image" width="100px"><br>
+              <svg id="device001img" class="device-image" width="100" height="100" xmlns="http://www.w3.org/2000/svg">
+                <g>
+                  <title>Client (1)</title>
+                  <path d="m 20,10 l 60,0 l 0,45 l -60,0 l 0,-45 z" fill="none" stroke-width="5" stroke="#000"/>
+                  <path d="m 20,60 l -15,30 l 90,0 l -15,-30" stroke="#000" fill="#fff" stroke-width="5"/>
+                  <path d="m 28,65 l  -8,16 l 60,0 l  -8,-16  z" stroke="#000" fill="#000" stroke-width="5"/>
+                  <text fill="#000000" stroke="#000" stroke-width="0" x="23.40522" y="19.58995" font-size="6" font-family="Noto Sans JP" text-anchor="start" xml:space="preserve">PS C:\&gt;_</text>
+                  <line fill="none" stroke="#fff" x1="20" y1="69" x2="80" y2="69"/>
+                  <line fill="none" stroke="#fff" x1="15" y1="76" x2="85" y2="76"/>
+                  <line fill="none" stroke="#fff" x1="36.60287" y1="60" x2="29.42583" y2="85"/>
+                  <line fill="none" stroke="#fff" x1="46.60287" y1="60" x2="45.42583" y2="85"/>
+                  <line fill="none" stroke="#fff" x1="55.60287" y1="60" x2="60.42583" y2="85"/>
+                  <line fill="none" stroke="#fff" x1="65.5083" y1="60" x2="72.68534" y2="85"/>
+                </g>
+              </svg>
+              <br>
               Client (1)<br>
               IPv4: 10.0.0.1<br>
               Subnetmask: 255.255.255.0
@@ -84,7 +99,22 @@ module NetworkSimulator =
             </div>
             
             <div id="device002" class="device device-note">
-              <img id="device002img" src="../image/client.png" class="device-image" width="100px"><br>
+              <svg id="device002img" class="device-image" width="100" height="100" xmlns="http://www.w3.org/2000/svg">
+                <g>
+                  <title>Client (2)</title>
+                  <path d="m 20,10 l 60,0 l 0,45 l -60,0 l 0,-45 z" fill="none" stroke-width="5" stroke="#000"/>
+                  <path d="m 20,60 l -15,30 l 90,0 l -15,-30" stroke="#000" fill="#fff" stroke-width="5"/>
+                  <path d="m 28,65 l  -8,16 l 60,0 l  -8,-16  z" stroke="#000" fill="#000" stroke-width="5"/>
+                  <text fill="#000000" stroke="#000" stroke-width="0" x="23.40522" y="19.58995" font-size="6" font-family="Noto Sans JP" text-anchor="start" xml:space="preserve">PS C:\&gt;_</text>
+                  <line fill="none" stroke="#fff" x1="20" y1="69" x2="80" y2="69"/>
+                  <line fill="none" stroke="#fff" x1="15" y1="76" x2="85" y2="76"/>
+                  <line fill="none" stroke="#fff" x1="36.60287" y1="60" x2="29.42583" y2="85"/>
+                  <line fill="none" stroke="#fff" x1="46.60287" y1="60" x2="45.42583" y2="85"/>
+                  <line fill="none" stroke="#fff" x1="55.60287" y1="60" x2="60.42583" y2="85"/>
+                  <line fill="none" stroke="#fff" x1="65.5083" y1="60" x2="72.68534" y2="85"/>
+                </g>
+              </svg>
+              <br>
               Client (2)<br>
               IPv4: 10.0.0.2<br>
               Subnetmask: 255.255.255.0
@@ -95,7 +125,17 @@ module NetworkSimulator =
             </div>
             
             <div id="device003" class="device device-note">
-              <img id="device003img" src="../image/router.png" class="device-image" width="100px"><br>
+              <svg id="device005img" class="device-image" width="100" height="35" xmlns="http://www.w3.org/2000/svg">
+                <g>
+                  <title>Router (1)</title>
+                  <path d="m 50,0 l 50,0 l 0,35 l -100,0 l 0,-35 l 50,0 z" stroke="#000000" fill="#000000"/>
+                  <path d="m 20,15 l 3,0 l 0,3 l 3,0 l 0,7 l -10,0 l 0,-7 l 3,0 l 0,-3 l 3,0 z" stroke="#000000" fill="#ffffff"/>
+                  <path d="m 40,15 l 3,0 l 0,3 l 3,0 l 0,7 l -10,0 l 0,-7 l 3,0 l 0,-3 l 3,0 z" stroke="#000000" fill="#ffffff"/>
+                  <path d="m 60,15 l 3,0 l 0,3 l 3,0 l 0,7 l -10,0 l 0,-7 l 3,0 l 0,-3 l 3,0 z" stroke="#000000" fill="#ffffff"/>
+                  <path d="m 80,15 l 3,0 l 0,3 l 3,0 l 0,7 l -10,0 l 0,-7 l 3,0 l 0,-3 l 3,0 z" stroke="#000000" fill="#ffffff"/>
+                </g>
+              </svg>
+              <br>
               Router (1)<br>
               IPv4: 10.0.0.3<br>
               Subnetmask: 255.255.255.0
@@ -106,7 +146,22 @@ module NetworkSimulator =
             </div>
             
             <div id="device004" class="device device-note">
-              <img id="device004img" src="../image/client.png" class="device-image" width="100px"><br>
+              <svg id="device004img" class="device-image" width="100" height="100" xmlns="http://www.w3.org/2000/svg">
+                <g>
+                  <title>Client (3)</title>
+                  <path d="m 20,10 l 60,0 l 0,45 l -60,0 l 0,-45 z" fill="none" stroke-width="5" stroke="#000"/>
+                  <path d="m 20,60 l -15,30 l 90,0 l -15,-30" stroke="#000" fill="#fff" stroke-width="5"/>
+                  <path d="m 28,65 l  -8,16 l 60,0 l  -8,-16  z" stroke="#000" fill="#000" stroke-width="5"/>
+                  <text fill="#000000" stroke="#000" stroke-width="0" x="23.40522" y="19.58995" font-size="6" font-family="Noto Sans JP" text-anchor="start" xml:space="preserve">PS C:\&gt;_</text>
+                  <line fill="none" stroke="#fff" x1="20" y1="69" x2="80" y2="69"/>
+                  <line fill="none" stroke="#fff" x1="15" y1="76" x2="85" y2="76"/>
+                  <line fill="none" stroke="#fff" x1="36.60287" y1="60" x2="29.42583" y2="85"/>
+                  <line fill="none" stroke="#fff" x1="46.60287" y1="60" x2="45.42583" y2="85"/>
+                  <line fill="none" stroke="#fff" x1="55.60287" y1="60" x2="60.42583" y2="85"/>
+                  <line fill="none" stroke="#fff" x1="65.5083" y1="60" x2="72.68534" y2="85"/>
+                </g>
+              </svg>
+              <br>
               Client (3)<br>
               IPv4: 10.0.0.18<br>
               Subnetmask: 255.255.255.240
@@ -117,7 +172,17 @@ module NetworkSimulator =
             </div>
             
             <div id="device005" class="device device-note">
-              <img id="device005img" src="../image/router.png" class="device-image" width="100px"><br>
+              <svg id="device005img" class="device-image" width="100" height="35" xmlns="http://www.w3.org/2000/svg">
+                <g>
+                  <title>Router (2)</title>
+                  <path d="m 50,0 l 50,0 l 0,35 l -100,0 l 0,-35 l 50,0 z" stroke="#000000" fill="#000000"/>
+                  <path d="m 20,15 l 3,0 l 0,3 l 3,0 l 0,7 l -10,0 l 0,-7 l 3,0 l 0,-3 l 3,0 z" stroke="#000000" fill="#ffffff"/>
+                  <path d="m 40,15 l 3,0 l 0,3 l 3,0 l 0,7 l -10,0 l 0,-7 l 3,0 l 0,-3 l 3,0 z" stroke="#000000" fill="#ffffff"/>
+                  <path d="m 60,15 l 3,0 l 0,3 l 3,0 l 0,7 l -10,0 l 0,-7 l 3,0 l 0,-3 l 3,0 z" stroke="#000000" fill="#ffffff"/>
+                  <path d="m 80,15 l 3,0 l 0,3 l 3,0 l 0,7 l -10,0 l 0,-7 l 3,0 l 0,-3 l 3,0 z" stroke="#000000" fill="#ffffff"/>
+                </g>
+              </svg>
+              <br>
               Router (2)<br>
               IPv4: 10.0.0.17<br>
               Subnetmask: 255.255.255.240
@@ -128,7 +193,22 @@ module NetworkSimulator =
             </div>
             
             <div id="device006" class="device device-note">
-              <img id="device006img" src="../image/client.png" class="device-image" width="100px"><br>
+              <svg id="device006img" class="device-image" width="100" height="100" xmlns="http://www.w3.org/2000/svg">
+                <g>
+                  <title>Client (4)</title>
+                  <path d="m 20,10 l 60,0 l 0,45 l -60,0 l 0,-45 z" fill="none" stroke-width="5" stroke="#000"/>
+                  <path d="m 20,60 l -15,30 l 90,0 l -15,-30" stroke="#000" fill="#fff" stroke-width="5"/>
+                  <path d="m 28,65 l  -8,16 l 60,0 l  -8,-16  z" stroke="#000" fill="#000" stroke-width="5"/>
+                  <text fill="#000000" stroke="#000" stroke-width="0" x="23.40522" y="19.58995" font-size="6" font-family="Noto Sans JP" text-anchor="start" xml:space="preserve">PS C:\&gt;_</text>
+                  <line fill="none" stroke="#fff" x1="20" y1="69" x2="80" y2="69"/>
+                  <line fill="none" stroke="#fff" x1="15" y1="76" x2="85" y2="76"/>
+                  <line fill="none" stroke="#fff" x1="36.60287" y1="60" x2="29.42583" y2="85"/>
+                  <line fill="none" stroke="#fff" x1="46.60287" y1="60" x2="45.42583" y2="85"/>
+                  <line fill="none" stroke="#fff" x1="55.60287" y1="60" x2="60.42583" y2="85"/>
+                  <line fill="none" stroke="#fff" x1="65.5083" y1="60" x2="72.68534" y2="85"/>
+                </g>
+              </svg>
+              <br>
               Client (4)<br>
               IPv4: 10.0.0.19<br>
               Subnetmask: 255.255.255.240
@@ -161,50 +241,15 @@ module NetworkSimulator =
                 <polyline stroke="#00aeda" stroke-width="5" fill="none" points="2,5 2,95"/>
               </svg>
             </div>"""
-
-        let devices =
-            document.getElementById("playArea").getElementsByTagName("img")
-            |> (fun x -> JS.Constructors.Array?from(x))
-            |> Array.filter (fun (x: Browser.Types.HTMLImageElement) -> x.classList.contains "lan-cable" = false)
-            |> Array.toList
         
-        let lanCables =
+        let lanCables, devices =
             document.getElementById("playArea").getElementsByTagName("svg")
             |> (fun x -> JS.Constructors.Array?from(x))
-            |> Array.filter (fun (x: Browser.Types.HTMLElement) -> x.classList.contains "lan-cable")
             |> Array.toList
+            |> List.partition (fun (x: Browser.Types.HTMLElement) -> x.classList.contains "lan-cable")
         
-//        [
-//            "device001img"
-//            "device002img"
-//            "device003img"
-//            "device004img"
-//            "device005img"
-//            "device006img"
-//            "lancable001img"
-//            "lancable002img"
-//            "lancable003img"
-//            "lancable004img"
-//        ]
-//        |> List.map document.getElementById
-        devices
-        |> List.iter (fun x ->
-            x.ondragstart <- fun _ -> false
-            x.onmousedown <- fun _ ->
-                //printfn "mouse down!"
-                let onMouseMove (event: Browser.Types.Event) =
-                    //printfn "mouse move!"
-                    let event = event :?> Browser.Types.MouseEvent
-                    let top = (event.clientY - x.getBoundingClientRect().height / 2.)
-                    let left = (event.clientX - x.getBoundingClientRect().width / 2.)
-                    let styleString = sprintf "%s: %fpx; %s: %fpx;" "top" top "left" left
-                    x.parentElement.setAttribute("style", styleString)
-                document.addEventListener("mousemove", onMouseMove)
-                x.onmouseup <- fun _ ->
-                    //printfn "mouse up!"
-                    document.removeEventListener("mousemove", onMouseMove))
-        
-        lanCables
+        (devices, lanCables)
+        ||> List.append
         |> List.iter (fun x ->
             x.ondragstart <- fun _ -> false
             x.onmousedown <- fun _ ->
@@ -242,7 +287,7 @@ module NetworkSimulator =
                 |> (fun x -> outputArea.innerHTML <- x)
                 |> ignore
             
-            let neighbors: Browser.Types.HTMLImageElement list =
+            let neighbors: Browser.Types.HTMLElement list =
                 getNetNeighbors lanCablesConnectedToClient001 devices source
 
             neighbors
