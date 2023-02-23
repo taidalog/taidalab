17bd6fdd456a06c6caa5753c396bc72ede224566
taidalog <72747279+taidalog@users.noreply.github.com>
2023-02-23 13:01:55 +0900

    Added a new member and conversion functions. #179

--- src/network-simulator/Cable.fs
+++ src/network-simulator/Cable.fs
@@ -5,13 +5,59 @@
 // https://github.com/taidalog/taidalab/blob/main/LICENSE
 namespace Taidalab
 
+open Browser.Dom
+open Fable.Core
+open Fable.Core.JsInterop
+
 type Cable =
-    { Kind : Kind
+    { Id : string
+      Kind : Kind
       Area : Area
-      Name : string }
+      Name : string
+      Points : string }
 
 module Cable =
-    let create kind area name =
-        { Cable.Kind = kind
+    let create id kind area name points : Cable =
+        { Cable.Id = id
+          Cable.Kind = kind
           Cable.Area = area
-          Cable.Name = name }
+          Cable.Name = name
+          Cable.Points = points }
+    
+    let toElement (cable: Cable) : string =
+        $"""
+        <div id="%s{cable.Id}" class="device cable-container lan-cable">
+            <svg id="%s{cable.Id}Svg" class="device lan-cable" viewBox="%f{cable.Area.X} %f{cable.Area.Y} %f{cable.Area.Width} %f{cable.Area.Height}" width="%f{cable.Area.Width}px" height="%f{cable.Area.Height}px" xmlns="http://www.w3.org/2000/svg">
+                <g>
+                    <title>%s{cable.Name}</title>
+                    <polyline stroke="#00aeda" stroke-width="5" fill="none" points="%s{cable.Points}"/>
+                </g>
+            </svg>
+            <span id="%s{cable.Id}Name" class="no-display">%s{cable.Name}</span>
+            <span id="%s{cable.Id}Kind" class="no-display">%s{cable.Kind |> string}</span>
+        </div>
+        """
+    
+    let ofHTMLElement (elm: Browser.Types.HTMLElement) : Cable option=
+        let id = elm.id
+        
+        let name = document.getElementById(id + "Name").innerText
+        
+        let kind =
+            document.getElementById(id + "Kind").innerText
+            |> Kind.ofString
+        
+        let area =
+            let svg = document.getElementById(id + "Svg")
+            let rect = svg.getBoundingClientRect()
+            Area.ofFloats rect.left rect.top rect.width rect.height
+        
+        let points =
+            elm.getElementsByTagName("polyline")
+            |> fun x -> JS.Constructors.Array?from(x)
+            |> Array.item 0
+            |> fun (x: Browser.Types.HTMLElement) -> x.getAttribute("points")
+        
+        match kind with
+        | Some x -> Some (create id x area name points)
+        | None -> None
--- src/network-simulator/Device.fs
+++ src/network-simulator/Device.fs
@@ -5,8 +5,11 @@
 // https://github.com/taidalog/taidalab/blob/main/LICENSE
 namespace Taidalab
 
+open Browser.Dom
+
 type Device =
-    { Kind : Kind
+    { Id : string
+      Kind : Kind
       Area : Area
       Name : string
       IPv4 : IPv4
@@ -14,14 +17,85 @@ type Device =
       NetworkAddress : IPv4 }
 
 module Device =
-    let create kind area name ipv4 subnetMask =
+    let create id kind area name ipv4 subnetMask : Device =
         let ipv4 = IPv4.ofDotDecimal ipv4
         let subnetMask = IPv4.ofDotDecimal subnetMask
         let networkAddress = IPv4.getSubnet subnetMask ipv4
 
-        { Device.Kind = kind
+        { Device.Id = id
+          Device.Kind = kind
           Device.Area = area
           Device.Name = name
           Device.IPv4 = ipv4
           Device.SubnetMask = subnetMask
           Device.NetworkAddress =  networkAddress }
+
+    let toElement (device: Device) : string =
+        match device.Kind with
+        | Kind.Client ->
+            $"""
+            <div id="%s{device.Id}" class="device device-container device-note">
+                <svg id="%s{device.Id}Svg" class="device-image" width="100" height="100" xmlns="http://www.w3.org/2000/svg">
+                    <g>
+                    <title>%s{device.Name}</title>
+                    <path d="m 20,10 l 60,0 l 0,45 l -60,0 l 0,-45 z" fill="none" stroke-width="5" stroke="#000"/>
+                    <path d="m 20,60 l -15,30 l 90,0 l -15,-30" stroke="#000" fill="#fff" stroke-width="5"/>
+                    <path d="m 28,65 l  -8,16 l 60,0 l  -8,-16  z" stroke="#000" fill="#000" stroke-width="5"/>
+                    <text fill="#000000" stroke="#000" stroke-width="0" x="23.40522" y="19.58995" font-size="6" font-family="Noto Sans JP" text-anchor="start" xml:space="preserve">PS C:\&gt;_</text>
+                    <line fill="none" stroke="#fff" x1="20" y1="69" x2="80" y2="69"/>
+                    <line fill="none" stroke="#fff" x1="15" y1="76" x2="85" y2="76"/>
+                    <line fill="none" stroke="#fff" x1="36.60287" y1="60" x2="29.42583" y2="85"/>
+                    <line fill="none" stroke="#fff" x1="46.60287" y1="60" x2="45.42583" y2="85"/>
+                    <line fill="none" stroke="#fff" x1="55.60287" y1="60" x2="60.42583" y2="85"/>
+                    <line fill="none" stroke="#fff" x1="65.5083" y1="60" x2="72.68534" y2="85"/>
+                    </g>
+                </svg>
+                <br>
+                <span id="%s{device.Id}Name" class="">%s{device.Name}</span><br>
+                IPv4: <span id="%s{device.Id}IPv4" class="">%s{device.IPv4.ToString()}</span><br>
+                SubnetMask: <span id="%s{device.Id}SubnetMask" class="">%s{device.SubnetMask.ToString()}</span>
+                <span id="%s{device.Id}Kind" class="no-display">%s{device.Kind |> string}</span>
+            </div>"""
+        | Kind.Router ->
+            $"""
+            <div id="%s{device.Id}" class="device device-container device-note">
+              <svg id="%s{device.Id}Svg" class="device-image" width="100" height="35" xmlns="http://www.w3.org/2000/svg">
+                <g>
+                  <title>%s{device.Name}</title>
+                  <path d="m 50,0 l 50,0 l 0,35 l -100,0 l 0,-35 l 50,0 z" stroke="#000000" fill="#000000"/>
+                  <path d="m 20,15 l 3,0 l 0,3 l 3,0 l 0,7 l -10,0 l 0,-7 l 3,0 l 0,-3 l 3,0 z" stroke="#000000" fill="#ffffff"/>
+                  <path d="m 40,15 l 3,0 l 0,3 l 3,0 l 0,7 l -10,0 l 0,-7 l 3,0 l 0,-3 l 3,0 z" stroke="#000000" fill="#ffffff"/>
+                  <path d="m 60,15 l 3,0 l 0,3 l 3,0 l 0,7 l -10,0 l 0,-7 l 3,0 l 0,-3 l 3,0 z" stroke="#000000" fill="#ffffff"/>
+                  <path d="m 80,15 l 3,0 l 0,3 l 3,0 l 0,7 l -10,0 l 0,-7 l 3,0 l 0,-3 l 3,0 z" stroke="#000000" fill="#ffffff"/>
+                </g>
+              </svg>
+              <br>
+              <span id="%s{device.Id}Name" class="">%s{device.Name}</span><br>
+              IPv4: <span id="%s{device.Id}IPv4" class="">%s{device.IPv4.ToString()}</span><br>
+              SubnetMask: <span id="%s{device.Id}SubnetMask" class="">%s{device.SubnetMask.ToString()}</span>
+              <span id="%s{device.Id}Kind" class="no-display">%s{device.Kind |> string}</span>
+            </div>
+            """
+        | _ -> ""
+    
+    let ofHTMLElement (elm: Browser.Types.HTMLElement) : Device option =
+        let id = elm.id
+        
+        let name = document.getElementById(id + "Name").innerText
+        
+        let ipv4 = document.getElementById(id + "IPv4").innerText
+        
+        let subnetMask = document.getElementById(id + "SubnetMask").innerText
+        
+        let kind =
+            document.getElementById(id + "Kind").innerText
+            |> Kind.ofString
+        
+        let area =
+            let svg = document.getElementById(id + "Svg")
+            let rect = svg.getBoundingClientRect()
+            Area.ofFloats rect.left rect.top rect.width rect.height
+
+        match kind with
+        | Some (x: Kind) -> Some (create id x area name ipv4 subnetMask)
+        | None -> None
