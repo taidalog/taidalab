8c55c192bdbc592425b35b88cc8bf76b758b2294
taidalog <72747279+taidalog@users.noreply.github.com>
2023-03-04 11:35:33 +0900

    Added the Point type. #179

--- src/network-simulator/Point.fs
+++ src/network-simulator/Point.fs
@@ -5,6 +5,8 @@
 // https://github.com/taidalog/taidalab/blob/main/LICENSE
 namespace Taidalab
 
+open System
+
 type Point =
     { X : float
       Y : float }
@@ -13,3 +15,15 @@ module Point =
     let ofFloats x y =
         { Point.X = x
           Point.Y = y }
+    
+    let ofString (str: string) : Point =
+        str.Split([|','|])
+        |> Array.map float
+        |> fun xs -> ofFloats (Array.head xs) (Array.last xs)
+    
+    let distance (p1: Point) (p2: Point) : float =
+        Math.Sqrt((p1.X - p2.X) ** 2 + (p1.Y - p2.Y) ** 2)
+    
+    let shift (x: float) (y: float) (point: Point) : Point =
+        { Point.X = point.X + x
+          Point.Y = point.Y + y }
