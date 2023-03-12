e8819397b0599a2ae8632bc27737d0d7334a5dd0
taidalog <72747279+taidalog@users.noreply.github.com>
2023-03-12 12:30:00 +0900

    Simplified `getNetNeighbor` function. #179

--- src/network-simulator/TCPIP.fs
+++ src/network-simulator/TCPIP.fs
@@ -15,33 +15,28 @@ module TCPIP =
             cables |> List.filter (fun c -> c.Area |> Area.isOver 0. sourceArea)
 //        connectedCables |> List.iter (fun x -> printfn "getNetNeighbor': source is connected to %s" x.Name)
         
-        let connectedClientsAndRouters =
-            connectedCables
-            |> List.collect
-                (fun c ->
-//                    printfn "%s is connected to below:" c.Name
-                    devices
-                    |> List.filter (fun d -> Device.isClient d || Device.isRouter d)
-                    |> List.filter (fun d ->
-                        //printfn "%s (%b)" d.Name (Area.isOver 0. d.Area c.Area && d.IPv4 <> source.IPv4)
-                        let dArea = d |> Device.getArea
-                        let dIPv4 = d |> Device.getIPv4AsList
-                        Area.isOver 0. dArea c.Area &&
-                        dIPv4 |> List.forall (fun x -> (Device.hasIPv4 x source) = false)))
+        let clientTester (source: Device) (cable: Cable) (client: Client) : bool =
+            Area.isOver 0. client.Area cable.Area &&
+            client.Id <> (Device.getId source)
         
-        let connectedHubs =
-            connectedCables
-            |> List.collect
-                (fun c ->
-                    devices
-                    |> List.filter Device.isHub
-                    |> List.filter (fun d ->
-                        let dArea = d |> Device.getArea
-                        Area.isOver 0. dArea c.Area &&
-                        (Device.getId d) <> (Device.getId source)))
-
+        let routerTester (source: Device) (cable: Cable) (router: Router) : bool =
+            Area.isOver 0. router.Area cable.Area &&
+            router.Id <> (Device.getId source)
         
-        List.concat [connectedClientsAndRouters; connectedHubs]
+        let hubTester (source: Device) (cable: Cable) (hub: Hub) : bool =
+            Area.isOver 0. hub.Area cable.Area &&
+            hub.Id <> (Device.getId source)
+        
+        connectedCables
+        |> List.collect
+            (fun c ->
+//                printfn "%s is connected to below:" c.Name
+                devices
+                |> List.filter (fun x ->
+                    match x with
+                    | Client d -> clientTester source c d
+                    | Router d -> routerTester source c d
+                    | Hub d -> hubTester source c d))
 
     let rec ping (cables: Cable list) (devices: Device list) (source: Device) (ttl: int) (destinationIPv4: IPv4) : bool =
         let neighbors = getNetNeighbors cables devices source
@@ -51,6 +46,7 @@ module TCPIP =
             | Router d -> d.Name
             | Hub d -> d.Name
 
+        printfn "ping starts."
         neighbors |> List.iter (fun x -> printfn "ping: %s is connected to %s" (sourceName source) (sourceName x))
         
         let found = neighbors |> List.exists (Device.hasIPv4 destinationIPv4)// (fun x -> x.IPv4 = destinationIPv4)
