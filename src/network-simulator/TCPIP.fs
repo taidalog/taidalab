a9aba578f48f5e0c3418ccd00439eb3a2e9c0580
taidalog <72747279+taidalog@users.noreply.github.com>
2023-03-12 19:28:08 +0900

    Modified Hub not to connect different NW. #179

--- src/network-simulator/TCPIP.fs
+++ src/network-simulator/TCPIP.fs
@@ -6,56 +6,59 @@
 namespace Taidalab
 
 module TCPIP =
-    let getNetNeighbors (cables: Cable list) (devices: Device list) (source: Device) : Device list =
+    let getNetNeighbors (cables: Cable list) (devices: Device list) (source: Device) (previous: Device) : Device list =
         //cables |> List.length |> printfn "%d cables."
         //cables |> List.iter (fun x -> printfn "%s (%b)" x.Name (x.Area |> Area.isOver 0. source.Area))
-        let sourceArea = source |> Device.getArea
+//        printfn "getNetNeighbors starts."
+//        printfn "getNetNeighbors: source:\t%s" (source |> Device.name)
+//        printfn "getNetNeighbors: previous:\t%s" (previous |> Device.name)
+
+        let sourceArea = source |> Device.area
 //        printfn "%A" sourceArea
         let connectedCables =
             cables |> List.filter (fun c -> c.Area |> Area.isOver 0. sourceArea)
 //        connectedCables |> List.iter (fun x -> printfn "getNetNeighbor': source is connected to %s" x.Name)
         
-        let clientTester (source: Device) (cable: Cable) (client: Client) : bool =
-            Area.isOver 0. client.Area cable.Area &&
-            client.Id <> (Device.getId source)
+        let clientTester (source: Device) (cable: Cable) (device: Device) : bool =
+            (Device.id device) <> (Device.id source) &&
+            Area.isOver 0. (Device.area device) cable.Area
         
-        let routerTester (source: Device) (cable: Cable) (router: Router) : bool =
-            Area.isOver 0. router.Area cable.Area &&
-            router.Id <> (Device.getId source)
+        let routerTester (source: Device) (cable: Cable) (device: Device) : bool =
+            (Device.id device) <> (Device.id source) &&
+            Area.isOver 0. (Device.area device) cable.Area
         
-        let hubTester (source: Device) (cable: Cable) (hub: Hub) : bool =
-            Area.isOver 0. hub.Area cable.Area &&
-            hub.Id <> (Device.getId source)
+        let hubTester (source: Device) (previous: Device) (cable: Cable) (device: Device) : bool =
+            (Device.id device) <> (Device.id source) &&
+            Area.isOver 0. (Device.area device) cable.Area &&
+            (List.intersection (Device.NetworkAddresses previous) (Device.NetworkAddresses device)) <> []
         
         connectedCables
         |> List.collect
             (fun c ->
 //                printfn "%s is connected to below:" c.Name
                 devices
-                |> List.filter (fun x ->
-                    match x with
-                    | Client d -> clientTester source c d
-                    | Router d -> routerTester source c d
-                    | Hub d -> hubTester source c d))
+                |> List.filter (fun d ->
+                    match source with
+                    | Client _ -> clientTester source c d
+                    | Router _ -> routerTester source c d
+                    | Hub _ -> hubTester source previous c d))
 
-    let rec ping (cables: Cable list) (devices: Device list) (source: Device) (ttl: int) (destinationIPv4: IPv4) : bool =
-        let neighbors = getNetNeighbors cables devices source
-        let sourceName device =
-            match device with
-            | Client d -> d.Name
-            | Router d -> d.Name
-            | Hub d -> d.Name
+    let ping (cables: Cable list) (devices: Device list) (source: Device) (ttl: int) (destinationIPv4: IPv4) : bool =
+        let rec ping' (cables: Cable list) (devices: Device list) (source: Device) (previous: Device) (ttl: int) (destinationIPv4: IPv4) : bool =
+            let neighbors = getNetNeighbors cables devices source previous
+            
+//            printfn "ping starts."
+//            printfn "previous: %s" (previous |> Device.name)
+//            neighbors |> List.iter (fun x -> printfn "ping: %s is connected to %s" (Device.name source) (Device.name x))
+            
+            let found = neighbors |> List.exists (Device.hasIPv4 destinationIPv4)
+            //printfn "ping: destination IPv4 found = %b" found
 
-        printfn "ping starts."
-        neighbors |> List.iter (fun x -> printfn "ping: %s is connected to %s" (sourceName source) (sourceName x))
-        
-        let found = neighbors |> List.exists (Device.hasIPv4 destinationIPv4)// (fun x -> x.IPv4 = destinationIPv4)
-        //printfn "ping: destination IPv4 found = %b" found
-
-        if found then
-            true
-        else if ttl = 0 then
-            false
-        else
-            neighbors
-            |> List.exists (fun x -> ping cables devices x (ttl - 1) destinationIPv4)
+            if found then
+                true
+            else if ttl = 0 then
+                false
+            else
+                neighbors
+                |> List.exists (fun x -> ping' cables devices x source (ttl - 1) destinationIPv4)
+        ping' cables devices source source ttl destinationIPv4
