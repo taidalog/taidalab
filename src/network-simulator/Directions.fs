4d4e3bae9d05650412e88edb5ffa9ce8750d2334
taidalog <72747279+taidalog@users.noreply.github.com>
2023-03-19 00:19:32 +0900

    Added a new enum. #179

---
+++ src/network-simulator/Directions.fs
@@ -0,0 +1,16 @@
+// taidalab Version 3.3.3
+// https://github.com/taidalog/taidalab
+// Copyright (c) 2022-2023 taidalog
+// This software is licensed under the MIT License.
+// https://github.com/taidalog/taidalab/blob/main/LICENSE
+namespace Taidalab
+
+open System
+
+[<Flags>]
+type Directions =
+    | None = 0
+    | Up = 1
+    | Down = 2
+    | Left = 4
+    | Right = 8
