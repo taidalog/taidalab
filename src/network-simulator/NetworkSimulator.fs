31f343314ad36224e257565616f43bb16da0f09d
taidalog <72747279+taidalog@users.noreply.github.com>
2023-03-19 21:11:41 +0900

    Improved functionality to modify cables. #179
    Finally cables can be dragged and modified in almost all angle.
    When it is dragged slowly, it turns its angle and length correctly.
    But it moves unexpectedly when dragged quickly,
    so there is need for more fix.

--- src/network-simulator/NetworkSimulator.fs
+++ src/network-simulator/NetworkSimulator.fs
@@ -86,6 +86,11 @@ module NetworkSimulator =
             else
                 (point2, point1)
 
+    let inline (|Positive|Negative|Zero|) (n: ^a) =
+        if n > LanguagePrimitives.GenericZero<'a> then Positive
+        else if n < LanguagePrimitives.GenericZero<'a> then Negative
+        else Zero
+    
     let resizeCable (container: Browser.Types.HTMLElement) (svg: Browser.Types.HTMLElement) (polyline: Browser.Types.HTMLElement) (event: Browser.Types.Event) : unit =
         let event = event :?> Browser.Types.MouseEvent
 
@@ -97,68 +102,72 @@ module NetworkSimulator =
             |> fun xs -> List.head xs, List.last xs
         
         let cursorPoint = Point.ofFloats (event.clientX - container.offsetLeft) (event.clientY - container.offsetTop)
-        printfn "point1:\t%O" point1
-        printfn "point2:\t%O" point2
-        printfn "cursorPoint:\t%O" cursorPoint
+//        printfn "point1:\t%O" point1
+//        printfn "point2:\t%O" point2
+//        printfn "cursorPoint:\t%O" cursorPoint
 
         let touchedPoint, untouchedPoint = cursorPoint |> touchedAndUntouched point1 point2
-        printfn "touchedPoint:\t%O" touchedPoint
-        printfn "untouchedPoint:\t%O" untouchedPoint
+//        printfn "touchedPoint:\t%O" touchedPoint
+//        printfn "untouchedPoint:\t%O" untouchedPoint
 
         let xMoving = cursorPoint.X - touchedPoint.X
         let yMoving = cursorPoint.Y - touchedPoint.Y
-        printfn "xMoving:\t%f, yMoving:\t%f" xMoving yMoving
+//        printfn "xMoving:\t%f, yMoving:\t%f" xMoving yMoving
 
         let touchedPointPosition = touchedPoint |> Point.relativePosition untouchedPoint
-        printfn "touchedPointPosition:\t%O" touchedPointPosition
         
         // Building the new end points with the cursor position.
         let updatedPoints =
             match touchedPointPosition with
             | Directions.Up ->
-                printfn "touchedPointRelativePosition:\tUp" |> ignore
+//                printfn "touchedPointRelativePosition:\tUp" |> ignore
                 touchedPoint, untouchedPoint |> Point.shift -xMoving -yMoving
             | Directions.Down ->
-                printfn "touchedPointRelativePosition:\tDown" |> ignore
+//                printfn "touchedPointRelativePosition:\tDown" |> ignore
                 cursorPoint |> updatePoints untouchedPoint touchedPoint
             | Directions.Left ->
-                printfn "touchedPointRelativePosition:\tLeft" |> ignore
+//                printfn "touchedPointRelativePosition:\tLeft" |> ignore
                 touchedPoint, untouchedPoint |> Point.shift -xMoving -yMoving
             | Directions.Right ->
-                printfn "touchedPointRelativePosition:\tRight" |> ignore
+//                printfn "touchedPointRelativePosition:\tRight" |> ignore
                 cursorPoint |> updatePoints untouchedPoint touchedPoint
             | var when var = (Directions.Up ||| Directions.Left) ->
-                printfn "touchedPointRelativePosition:\tUpLeft" |> ignore
+//                printfn "touchedPointRelativePosition:\tUpLeft" |> ignore
                 touchedPoint, untouchedPoint |> Point.shift -xMoving -yMoving
             | var when var = (Directions.Up ||| Directions.Right) ->
-                printfn "touchedPointRelativePosition:\tUpRight" |> ignore
+//                printfn "touchedPointRelativePosition:\tUpRight" |> ignore
                 untouchedPoint |> Point.shift 0. -yMoving, touchedPoint |> Point.shift xMoving 0.
             | var when var = (Directions.Down ||| Directions.Left) ->
-                printfn "touchedPointRelativePosition:\tDownLeft" |> ignore
+//                printfn "touchedPointRelativePosition:\tDownLeft" |> ignore
                 touchedPoint |> Point.shift 0. yMoving, untouchedPoint |> Point.shift -xMoving 0.
             | var when var = (Directions.Down ||| Directions.Right) ->
-                printfn "touchedPointRelativePosition:\tDownRight" |> ignore
+//                printfn "touchedPointRelativePosition:\tDownRight" |> ignore
                 cursorPoint |> updatePoints untouchedPoint touchedPoint
             | _ ->
-                printfn "touchedPointRelativePosition:\t_" |> ignore
+//                printfn "touchedPointRelativePosition:\t_" |> ignore
                 cursorPoint |> updatePoints untouchedPoint touchedPoint
         
-        printfn "updatedPoints:\t(%O), (%O)" <|| updatedPoints
+//        printfn "updatedPoints:\t(%O), (%O)" <|| updatedPoints
         
+        let xGap = updatedPoints |> Tuple.map (fun x -> x.X) |> System.Math.Min |> fun x -> 5. - x
+        let yGap = updatedPoints |> Tuple.map (fun x -> x.Y) |> System.Math.Min |> fun x -> 5. - x
+//        printfn "xGap:\t%f, yGap:\t%f" xGap yGap
+
         // Updating the cable points.
         updatedPoints
+        |> Tuple.map (Point.shift xGap yGap)
         |> fun (p1, p2) -> $"%f{p1.X},%f{p1.Y} %f{p2.X},%f{p2.Y}"
         |> fun x -> polyline.setAttribute("points", x)
 
 //        printfn "container.offsetTop:\t%f, container.offsetLeft:\t%f" container.offsetTop container.offsetLeft
 
-        let updatedArea = updatedPoints ||> Area.ofPoints |> Area.expand (5. * 2.) (5. * 2.)
+        let updatedArea = updatedPoints |> Tuple.map (Point.shift xGap yGap) ||> Area.ofPoints |> Area.expand (5. * 2.) (5. * 2.)
         svg.setAttribute("viewBox", $"0 0 %f{updatedArea.Width} %f{updatedArea.Height}")
         svg.setAttribute("width", $"%f{updatedArea.Width}px")
         svg.setAttribute("height", $"%f{updatedArea.Height}px")
-        svg.setAttribute("style", "background-color: red;")
+//        svg.setAttribute("style", "background-color: red;")
 
-        printfn "updatedArea:\t%O" <| updatedArea
+//        printfn "updatedArea:\t%O" <| updatedArea
 
         // Shifting the cable container.
         match touchedPointPosition with
@@ -178,7 +187,7 @@ module NetworkSimulator =
         | _ -> ()
         
         let touchedPointPosition' = updatedPoints ||> Point.relativePosition
-        printfn "touchedPointPosition':\t%O" touchedPointPosition'
+//        printfn "touchedPointPosition':\t%O" touchedPointPosition'
 
         // Resizing and shifting the cable container.
         match touchedPointPosition' with
@@ -200,7 +209,7 @@ module NetworkSimulator =
         //| var when var = (Directions.Down ||| Directions.Left) ->
         //| var when var = (Directions.Down ||| Directions.Right) ->
         | _ -> ()
-        printfn ""
+//        printfn ""
     
     let setMouseMoveEventCable (container: Browser.Types.HTMLElement) : unit =
         let cable = Cable.ofHTMLElement container
