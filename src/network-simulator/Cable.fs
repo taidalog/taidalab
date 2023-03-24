a6a41d6f67b7b3f40d44ba1f7636e9db2646e0b9
taidalog <72747279+taidalog@users.noreply.github.com>
2023-03-24 20:26:37 +0900

    Built the `Cable.connectedTo` function. #179
    This function tells whether a cable is connected to a device
    in fewer codes than with `Area.includesPoint`.

--- src/network-simulator/Cable.fs
+++ src/network-simulator/Cable.fs
@@ -32,19 +32,42 @@ module Cable =
           Cable.Area = area
           Cable.Position = position }
     
-//    let toElement (cable: Cable) : string =
-//        $"""
-//        <div id="%s{cable.Id}" class="device cable-container lan-cable" style="top: %f{cable.Position.Y}px; left: %f{cable.Position.X}px;">
-//            <svg id="%s{cable.Id}Svg" class="device lan-cable" viewBox="%f{cable.Area.X} %f{cable.Area.Y} %f{cable.Area.Width} %f{cable.Area.Height}" width="%f{cable.Area.Width}px" height="%f{cable.Area.Height}px" xmlns="http://www.w3.org/2000/svg">
-//                <g>
-//                    <title>%s{cable.Name}</title>
-//                    <polyline id="%s{cable.Id}Polyline" stroke="#00aeda" stroke-width="5" fill="none" points="%s{cable.Points}"/>
-//                </g>
-//            </svg>
-//            <span id="%s{cable.Id}Name" class="no-display">%s{cable.Name}</span>
-//            <span id="%s{cable.Id}Kind" class="no-display">%s{cable.Kind |> string}</span>
-//        </div>
-//        """
+    let ofHTMLElement (elm: Browser.Types.HTMLElement) : Cable option =
+        let id = elm.id
+        
+        let name = document.getElementById(id + "Name").innerText
+        
+        let kind =
+            document.getElementById(id + "Kind").innerText
+            |> Kind.ofString
+        
+        let area =
+            let svg = document.getElementById(id + "Svg")
+            let rect = svg.getBoundingClientRect()
+            Area.ofFloats rect.left rect.top rect.width rect.height
+        
+        let points =
+            document.getElementById(id + "Polyline")
+            |> fun (x: Browser.Types.HTMLElement) -> x.getAttribute("points")
+            |> String.split ' '
+            |> List.map Point.ofString
+        
+        let position =
+            let x =
+                elm.getAttribute("style")
+                |> Regex.match' """left: (\d+\.?\d+)px;"""
+                |> fun m -> (m.Groups.Item 1).Value
+                |> float
+            let y =
+                elm.getAttribute("style")
+                |> Regex.match' """top: (\d+\.?\d+)px;"""
+                |> fun m -> (m.Groups.Item 1).Value
+                |> float
+            Point.ofFloats x y
+
+        match kind with
+        | Some x -> Some (create id x name points area position)
+        | None -> None
     
     let toHTMLElement (cable: Cable) : Browser.Types.HTMLElement =
         let container = document.createElement("div")
@@ -100,39 +123,7 @@ module Cable =
 
         container
     
-    let ofHTMLElement (elm: Browser.Types.HTMLElement) : Cable option =
-        let id = elm.id
-        
-        let name = document.getElementById(id + "Name").innerText
-        
-        let kind =
-            document.getElementById(id + "Kind").innerText
-            |> Kind.ofString
-        
-        let area =
-            let svg = document.getElementById(id + "Svg")
-            let rect = svg.getBoundingClientRect()
-            Area.ofFloats rect.left rect.top rect.width rect.height
-        
-        let points =
-            document.getElementById(id + "Polyline")
-            |> fun (x: Browser.Types.HTMLElement) -> x.getAttribute("points")
-            |> String.split ' '
-            |> List.map Point.ofString
-        
-        let position =
-            let x =
-                elm.getAttribute("style")
-                |> Regex.match' """left: (\d+\.?\d+)px;"""
-                |> fun m -> (m.Groups.Item 1).Value
-                |> float
-            let y =
-                elm.getAttribute("style")
-                |> Regex.match' """top: (\d+\.?\d+)px;"""
-                |> fun m -> (m.Groups.Item 1).Value
-                |> float
-            Point.ofFloats x y
-
-        match kind with
-        | Some x -> Some (create id x name points area position)
-        | None -> None
+    let connedtedTo (device: Device) (cable: Cable) : bool =
+        cable.Points
+        |> List.map (Point.shift cable.Area.X cable.Area.Y)
+        |> List.exists (Area.includesPoint (Device.area device))
