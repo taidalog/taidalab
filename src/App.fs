cc61f31fc4663f66663e7b217c8e341cb7655e85
taidalog <72747279+taidalog@users.noreply.github.com>
2024-10-16 00:13:21 +0900

    Wrote new lines for routing and commented out old ones. #289

--- src/App.fs
+++ src/App.fs
@@ -8,46 +8,62 @@ namespace Taidalab
 open Fable.Core
 open Fable.Core.JsInterop
 open Browser.Dom
-open Browser.Types
+open Browser.Url
+// open Browser.Types
 open Taidalab.Switcher
 
 module Main =
+    let init () =
+        document.body.innerHTML <- ""
+        document.body.innerHTML <- Content.Common.body
+        (document.querySelector "footer").innerHTML <- Content.Common.footer
+        (document.querySelector "aside").innerHTML <- Content.Common.aside
 
     window.addEventListener (
         "DOMContentLoaded",
         (fun _ ->
-            printfn "%s" "The begining of DOMContentLoaded"
-            document.body.innerHTML <- Content.Common.body
-            (document.querySelector "footer").innerHTML <- Content.Common.footer
-            (document.querySelector "aside").innerHTML <- Content.Common.aside
-            printfn "%s" window.location.pathname
+            // printfn "%s" "The begining of DOMContentLoaded"
+            // document.body.innerHTML <- Content.Common.body
+            // (document.querySelector "footer").innerHTML <- Content.Common.footer
+            // (document.querySelector "aside").innerHTML <- Content.Common.aside
+            init ()
+            // printfn "%s" window.location.pathname
 
-            let pathname = window.location.pathname
-            pathname |> Switcher.initPageFromPathname |> ignore
+            // let pathname = window.location.pathname
+            // pathname |> Switcher.initPageFromPathname |> ignore
+            let mergedUrl = window.location.href |> URL.Create |> Url.mergePathname
+            window.history.replaceState (null, "", mergedUrl.href)
+            Switcher.init mergedUrl
 
-            (document.querySelector "aside").getElementsByTagName "a"
-            |> (fun x -> JS.Constructors.Array?from(x))
-            |> Array.toList
-            |> List.iter (Switcher.switchAnchorAction pathname)
-
-            printfn "%s" "The end of DOMContentLoaded")
+            // (document.querySelector "aside").getElementsByTagName "a"
+            // |> (fun x -> JS.Constructors.Array?from(x))
+            // |> Array.toList
+            // |> List.iter (Switcher.switchAnchorAction pathname)
+            document.links |> JS.Constructors.Array?from |> Array.iter overwriteAnchor
+        // printfn "%s" "The end of DOMContentLoaded"
+        )
     )
 
     window.addEventListener (
         "popstate",
         (fun _ ->
-            let links: HTMLAnchorElement array =
-                (document.querySelector "aside").getElementsByTagName "a"
-                |> (fun x -> JS.Constructors.Array?from(x))
-                |> Array.filter (fun (x: HTMLAnchorElement) -> x.id <> "asideSoon")
-                |> Array.filter (fun (x: HTMLAnchorElement) -> x.pathname <> "/taidalab/")
+            // let links: HTMLAnchorElement array =
+            //     (document.querySelector "aside").getElementsByTagName "a"
+            //     |> (fun x -> JS.Constructors.Array?from(x))
+            //     |> Array.filter (fun (x: HTMLAnchorElement) -> x.id <> "asideSoon")
+            //     |> Array.filter (fun (x: HTMLAnchorElement) -> x.pathname <> "/taidalab/")
 
-            links
-            |> Array.iter (fun (x: HTMLAnchorElement) -> x.classList.remove "current-location")
+            // links
+            // |> Array.iter (fun (x: HTMLAnchorElement) -> x.classList.remove "current-location")
 
-            links
-            |> Array.filter (fun (x: HTMLAnchorElement) -> x.pathname = window.location.pathname)
-            |> Array.iter (fun x -> x.classList.add "current-location")
+            // links
+            // |> Array.filter (fun (x: HTMLAnchorElement) -> x.pathname = window.location.pathname)
+            // |> Array.iter (fun x -> x.classList.add "current-location")
 
-            window.location.pathname |> Switcher.initPageFromPathname |> ignore)
+            // window.location.pathname |> Switcher.initPageFromPathname |> ignore
+
+            let mergedUrl = window.location.href |> URL.Create |> Url.mergePathname
+            window.history.replaceState (null, "", mergedUrl.href)
+            Switcher.init mergedUrl
+            document.links |> JS.Constructors.Array?from |> Array.iter overwriteAnchor)
     )
