1ae9d97a2ecfa237c57153838e6f83d8764a9835
taidalog <72747279+taidalog@users.noreply.github.com>
2024-10-15 23:51:53 +0900

    Created `Ur.fs`. #289
    This file contains a module to provide functions for operation with URLs.

---
+++ src/Url.fs
@@ -0,0 +1,40 @@
+// taidalab Version 4.6.3
+// https://github.com/taidalog/taidalab
+// Copyright (c) 2022-2024 taidalog
+// This software is licensed under the MIT License.
+// https://github.com/taidalog/taidalab/blob/main/LICENSE
+namespace Taidalab
+
+open Browser.Types
+open Browser.Url
+
+module Url =
+    let baseUrl: string =
+#if DEBUG
+        "http://localhost:8080"
+#else
+        "https://taidalog.github.io/"
+#endif
+
+    let mergePathname (url: URL) =
+        let searchParams = url.searchParams
+
+        match searchParams.get "pathname" with
+        | None -> url
+        | Some v ->
+            let searchParams' = url.searchParams
+            searchParams'.delete "pathname"
+
+            if searchParams'.ToString() = "" then
+                URL.Create(url.origin + v)
+            else
+                URL.Create(url.origin + v + "?" + searchParams'.ToString())
+
+    let isInternal (baseUrl: string) (url: URL) : bool =
+        url.origin = baseUrl && url.pathname.StartsWith("/taidalab/")
+
+    let isExternal (baseUrl: string) (url: URL) : bool = url |> isInternal baseUrl |> not
+
+    let isInternal' (url: URL) : bool = isInternal baseUrl url
+
+    let isExternal' (url: URL) : bool = url |> isInternal' |> not
