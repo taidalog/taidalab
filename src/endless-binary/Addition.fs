a245e97bdde072f2b2d2ed52390875fd1c777cf7
taidalog <72747279+taidalog@users.noreply.github.com>
2024-11-08 21:11:57 +0900

    Raised version to 5.0.1. #296

--- src/endless-binary/Addition.fs
+++ src/endless-binary/Addition.fs
@@ -1,4 +1,4 @@
-// taidalab Version 5.0.0
+// taidalab Version 5.0.1
 // https://github.com/taidalog/taidalab
 // Copyright (c) 2022-2024 taidalog
 // This software is licensed under the MIT License.
