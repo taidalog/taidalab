1c0b0ac0e0d6f04c24393b5fd41f9f58665b6957
taidalog <72747279+taidalog@users.noreply.github.com>
2025-07-30 20:41:28 +0900

    Type annotation and interepolated string. #335

--- src/common/Svg.fs
+++ src/common/Svg.fs
@@ -20,62 +20,57 @@ module Svg =
             durMs: int *
             repeatCount: string
 
-    let frame width height content =
-        sprintf
-            """
-            <?xml version="1.0" standalone="no"?>
-            <svg width="%d" height="%d" version="1.1" xmlns="http://www.w3.org/2000/svg">
-                %s
-            </svg>
-            """
-            width
-            height
-            content
+    let frame (width: int) (height: int) (content: string) : string =
+        $"""
+        <?xml version="1.0" standalone="no"?>
+        <svg width="%d{width}" height="%d{height}" version="1.1" xmlns="http://www.w3.org/2000/svg">
+            %s{content}
+        </svg>
+        """
 
-    let text x y opacity text =
-        sprintf
-            """<text x="%d" y="%d" font-family="Courier New" font-size="20" opacity="%f">%s</text>"""
-            x
-            y
-            opacity
-            text
+    let text (x: int) (y: int) (opacity: float) (text: string) : string =
+        $"""<text x="%d{x}" y="%d{y}" font-family="Courier New" font-size="20" opacity="%f{opacity}">%s{text}</text>"""
 
-    let path d stroke strokeWidth fill opacity animation =
-        sprintf
-            """<path d="%s" stroke="%s" stroke-width=%d fill="%s" opacity="%f">%s</path>"""
-            d
-            stroke
-            strokeWidth
-            fill
-            opacity
-            animation
+    let path
+        (d: string)
+        (stroke: string)
+        (strokeWidth: int)
+        (fill: string)
+        (opacity: float)
+        (animation: string)
+        : string =
+        $"""<path d="%s{d}" stroke="%s{stroke}" stroke-width=%d{strokeWidth} fill="%s{fill}" opacity="%f{opacity}">%s{animation}</path>"""
 
-    let polyline points stroke strokeWidth fill animation =
-        sprintf
-            """<polylie points="%s" stroke="%s" stroke-width=%d fill="%s">%s</polyline>"""
-            points
-            stroke
-            strokeWidth
-            fill
-            animation
+    let polyline (points: string) (stroke: string) (strokeWidth: int) (fill: string) (animation: string) : string =
+        $"""<polylie points="%s{points}" stroke="%s{stroke}" stroke-width=%d{strokeWidth} fill="%s{fill}">%s{animation}</polyline>"""
 
-    let animate attributeName calcMode fromState toState beginMs durMs repeatCount fill =
-        sprintf
-            """<animate attributeName="%s" calcMode="%s" from="%s" to="%s" begin="%dms" dur="%dms" repeatCount="%s" fill="%s" />"""
-            attributeName
-            calcMode
-            fromState
-            toState
-            beginMs
-            durMs
-            repeatCount
-            fill
+    let animate
+        (attributeName: string)
+        (calcMode: string)
+        (fromState: string)
+        (toState: string)
+        (beginMs: int)
+        (durMs: int)
+        (repeatCount: string)
+        (fill: string)
+        : string =
+        $"""<animate attributeName="%s{attributeName}" calcMode="%s{calcMode}" from="%s{fromState}" to="%s{toState}" begin="%d{beginMs}ms" dur="%d{durMs}ms" repeatCount="%s{repeatCount}" fill="%s{fill}" />"""
 
-    let animateOpacity beginMs durMs =
+    let animateOpacity (beginMs: int) (durMs: int) : string =
         animate "opacity" "linear" "0" "1" beginMs durMs "1" "freeze"
 
-    let newArrow x y width1 height1 width2 height2 beginMs stroke fill =
+    let newArrow
+        (x: float)
+        (y: float)
+        (width1: float)
+        (height1: float)
+        (width2: float)
+        (height2: float)
+        (beginMs: int)
+        (stroke: string)
+        (fill: string)
+        : string =
         let d =
-            sprintf "M %f,%f h %f v %f h -7 l 16,-20 16,20 h -7 v %f h %f Z" x y width1 height1 height2 width2
+            $"M %f{x},%f{y} h %f{width1} v %f{height1} h -7 l 16,-20 16,20 h -7 v %f{height2} h %f{width2} Z"
 
         path d stroke 1 fill 0. (animateOpacity beginMs 500)
